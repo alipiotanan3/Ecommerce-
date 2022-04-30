04f6d12f0e31e354a309d2121986b99f978053c0
Alipio <78184376+alipiotanan3@users.noreply.github.com>
2022-04-30 19:21:44 -0300

    iniciando projeto

--- .history/index_20220430185343.js
+++
@@ -1,7 +0,0 @@
-const express = require ('express');
-const app = express();
-const mongoose = require("")
-
-app.listen(5000, ()=>{
-    console.log("servidor rodando")
-})
--- .history/index_20220430185346.js
+++
@@ -1,7 +0,0 @@
-const express = require ('express');
-const app = express();
-const mongoose = require("mongii")
-
-app.listen(5000, ()=>{
-    console.log("servidor rodando")
-})
